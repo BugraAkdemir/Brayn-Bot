e9cd79e724b38c7d995e2609ff4ae89f0595c580
ZYLLES <156844892+Zylles@users.noreply.github.com>
2024-03-14 23:51:07 +0300

    Add files via upload

---
+++ src/insta.py
@@ -0,0 +1,24 @@
+import instaloader
+import pandas as pd
+
+# Creating an instance of the Instaloader class
+
+bot = instaloader.Instaloader()
+
+# Loading a profile from an Instagram handle
+
+profile = instaloader.Profile.from_username(bot.context, "s1r.bugra")
+
+print("Username: ", profile.username)
+
+print("User ID: ", profile.userid)
+
+print("Number of Posts: ", profile.mediacount)
+
+print("Followers Count: ", profile.followers)
+
+print("Following Count: ", profile.followees)
+
+print("Bio: ", profile.biography)
+
+print("External URL: ", profile.external_url)
