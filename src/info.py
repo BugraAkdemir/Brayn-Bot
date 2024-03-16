fd9bb04b150deb4cedecf68184c9b72a53fcb45a
ZYLLES <156844892+Zylles@users.noreply.github.com>
2024-03-16 03:35:12 +0300

    Delete src directory

--- src/info.py
+++
@@ -1,8 +0,0 @@
-bilgi = "YARDIM\n\nNORMAL KOMUTLAR\n!normal\n\nOyunlar İÇİN\n!oyunlar\n\nADMİN KOMUTLARI İÇİN\n!admin"
-oyunlar = "Zar Atma\n !oyun zar\n\nTAŞ KAĞIT MAKAS OYUNU\n!tkmk\n\nADAM ASMACA OYUNU İÇİN\n!adamasmaca\n\n"
-admink = "KULLANICIYI BANLAMAK İÇİN\n!ban @KULLANICI\n\nKULLANICININ BANINI KALDIRMAK İÇİN\n!unban @kullanıcı\n\nKULLANICI ATMA\n!kick @kullanıcı\n\nKULLANICI ROL VERME\n!rolver @kullancı @rol"
-normalb = "BOTUN PİNG'i\n!ping\n\nBOTUN DAVET LİNKİ\n!davet\n\nBOTUN SÜRÜMÜNÜ ÖĞRENMEK İÇİN\n!version --akd"
-tkmk = "OYNAMAK İÇİN LÜTFEN BİR NESNE ŞEÇİN\n!tkm makas\n!tkm taş\n!tkm kağıt"
-hata_m = "Eğer Komutlari Bilmiyorsan\n!yardim veya !help\n Komutlarini Kullan"
-#--------------------------------------------İNGİLİZCE-------------------------
-bilgi2 = "HELP\n\nNORMAL COMMANDS\n!basic\n\nFOR Games\n!games\n\nFOR ADMIN COMMANDS\n!admin"
